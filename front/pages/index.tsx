6127099d5374f207bedd76b0cfcacfb93a86c366
Igor Kamyshev <igor@kamyshev.me>
2018-12-27 19:32:07 +0300

    ci: add css linting and type checking

--- front/pages/index.tsx
+++ front/pages/index.tsx
@@ -3,7 +3,9 @@ import * as React from 'react'
 import log from '@front/log'
 import con from '@shared/module'
 
+import s from './index.css'
+
 export default () => {
   con()
-  return <p>{log()}</p>
+  return <p className={s.title}>{log()}</p>
 }
