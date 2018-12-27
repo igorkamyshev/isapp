bcc03d74ccea615e7787203ad2a3c57dbe855f25
Igor Kamyshev <igor@kamyshev.me>
2018-12-28 00:02:41 +0300

    build: replace @zeit/next-css by next-css-unpluggable

--- front/next.config.js
+++ front/next.config.js
@@ -1,5 +1,5 @@
 const withPlugins = require('next-compose-plugins')
-const withCSS = require('@zeit/next-css')
+const withCSS = require('next-css-unpluggable')
 const withTypescript = require('@zeit/next-typescript')
 
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
